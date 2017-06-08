e2a3aba3ba9eaaee5166679796c81dd3cc63ef45
Rob Prouse <rob@prouse.org>
2017-06-07 20:20:01 -0400

    Insert readings into database

--- ParseWeb/Program.cs
+++ ParseWeb/Program.cs
@@ -1,47 +1,47 @@
 ﻿using System;
+using System.Data.SqlClient;
+using Dapper;
 
 namespace ParseWeb
 {
     class Program
     {
+        const string UPDATE = "update Reading with (serializable) set [BeachId]=@BeachId, [Open]=@Open, [DateTested]=@DateTested, [DateAdded]=@DateAdded, [Temperature]=@Temperature, [Message]=@Message where [BeachId]=@BeachId and [DateAdded]=@DateAdded";
+        const string INSERT = "insert into Reading ([BeachId], [Open], [DateTested], [DateAdded], [Temperature], [Message]) values (@BeachId, @Open, @DateTested, @DateAdded, @Temperature, @Message)";
+
         static void Main(string[] args)
         {
             var readings = HamiltonBeaches.ParseBeachQualityPage().Result;
-
-            foreach(var reading in readings)
+            try
             {
-                switch (reading.Open)
+                using (SqlConnection connection = new SqlConnection($"Server=tcp:beaches.database.windows.net,1433;Initial Catalog=SafeBeaches;Persist Security Info=False;User ID={args[0]};Password={args[1]};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
                 {
-                    case OpenStatus.Untested:
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.WriteLine(reading.Beach.Name);
-                        Console.WriteLine($"\t{reading.Message}");
-                        break;
-                    case OpenStatus.Closed:
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine(reading.Beach.Name);
-                        Console.WriteLine($"\t{reading.Message}");
-                        break;
-                    case OpenStatus.Open:
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine(reading.Beach.Name);
-                        Console.WriteLine($"\tTested: {reading.DateTested.Value.ToString("d")}");
-                        Console.WriteLine($"\tTemperature: {reading.Temperature}");
-                        break;
-                    default:
-                        break;
+                    connection.Open();
+
+                    foreach (var reading in readings)
+                    {
+                        Console.WriteLine($"{reading.Beach.Name} is {reading.Open}");
+
+                        var parms = new { BeachId = reading.Beach.Id, Open = (int)reading.Open, DateTested = reading.DateTested, DateAdded = reading.DateAdded, Temperature = reading.Temperature, Message = reading.Message };
+                        var rows = connection.Execute(UPDATE, parms);
+                        if (rows == 0)
+                        {
+                            rows = connection.Execute(INSERT, parms);
+                        }
+                    }
                 }
-                Console.WriteLine();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
             }
 
             ConfirmExit();
-            Console.ResetColor();
         }
 
         private static void ConfirmExit()
         {
 #if DEBUG
-            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine();
             Console.WriteLine("*** Press ENTER to Exit ***");
             Console.ReadLine();
